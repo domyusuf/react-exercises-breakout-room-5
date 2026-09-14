ccbb47d13233009db261d0f6d2bb5ec3acf2323d
owen-stud123 <180330378+owen-stud123@users.noreply.github.com>
2026-09-14 11:06:08 +0200

    complete project setup

---
+++ breakout-room-5/src/MemberCard.tsx
@@ -0,0 +1,9 @@
+function MemberCard() {
+    return (
+       <div>
+        <h3>Team Member</h3>
+       </div>
+    );
+}
+
+export default MemberCard;
---
+++ breakout-room-5/src/TeamDashboard.tsx
@@ -0,0 +1,14 @@
+import MemberCard from './MemberCard';
+
+
+function TeamDashboard() {
+  return (
+    <>
+      <h1>Team Dashboard</h1>
+      <p>This application helps manage</p>
+        <MemberCard />
+    </>
+  );
+}
+
+export default TeamDashboard;
