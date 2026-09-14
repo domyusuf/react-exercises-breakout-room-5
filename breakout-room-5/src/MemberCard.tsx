4b6df424d87448e8197ab8ec5cc95a2a55aeba95
Dominion Yusuf <d.yusuf@alustudent.com>
2026-09-14 11:20:53 +0200

    completed tasks 11&12

--- breakout-room-5/src/MemberCard.tsx
+++ breakout-room-5/src/MemberCard.tsx
@@ -1,9 +1,20 @@
-function MemberCard() {
+// Author: Dominion Yusuf
+
+interface MemberCardProps {
+    name: string;
+    role: string;
+}
+
+
+function MemberCard({ name, role }: MemberCardProps) {
     return (
        <div>
-        <h3>Team Member</h3>
+        <h3>{name}</h3>
+        <p>Role: {role}</p>
        </div>
     );
 }
 
 export default MemberCard;
+
+
